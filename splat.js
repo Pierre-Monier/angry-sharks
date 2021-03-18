1cc3b12a56a8ca3f31cbf634ca32b96776f64191
Jolan <Jolan>
2021-03-18 11:26:38 +0100

    clean project

--- splat.js
+++ splat.js
@@ -95,7 +95,7 @@ Splat.prototype.setPosition = function(x,y,z) {
 Splat.prototype.setParameters = function(elapsed) {
     this.time += 0.01*elapsed;
     // on peut animer les splats ici. Par exemple : 
-    this.position[1] += 0.03; // permet de déplacer le splat vers le haut au fil du temps
+    //this.position[1] += 0.03; // permet de déplacer le splat vers le haut au fil du temps
     //this.position[0] += 0.02*Math.sin(this.time); // permet de déplacer le splat sur l'axe X
 }
 
