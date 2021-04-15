fea79101230da8130401c976861ebec82c594652
PIerre Monier <p.monier96@gmail.com>
2021-04-15 14:29:54 +0200

    feat(faas): add addScore and getBestScore functions

--- faas/functions/src/index.ts
+++ faas/functions/src/index.ts
@@ -1,9 +1,28 @@
 import * as functions from "firebase-functions";
+import * as fs from 'firebase-admin';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+const serviceAccount = require('../lptroisd-firebase-adminsdk.json');
+
+fs.initializeApp({
+  credential: fs.credential.cert(serviceAccount)
+});
+
+const db = fs.firestore();
+
+export const getBestScores = functions.https.onRequest(async (request, response) => {
+  const topScoresSnapshot = await db.collection('scores').orderBy("score", "desc").limit(5).get();
+  const topScores = topScoresSnapshot.docs.map((score) => score.data())
+
+  response.send(topScores);
+});
+
+export const addScore = functions.https.onRequest((request, response) => {
+  const dataScore = request.body.data;
+
+  db.collection('scores').add(dataScore).then(() => {
+    response.send('Score save :)');
+  }).catch(() => {
+    response.statusCode = 500;
+    response.send('An error occurred while adding the score :(');
+  })
 });
